9174fdd09f7220a4d29e538f38acefb84bcbf901
manoharn725 <manoharnmanu725@gmail.com>
2024-10-08 22:33:55 +0530

    habitForm

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 import store from "./store/store";
 import { Container, Typography } from "@mui/material";
+import AddHabitForm from "./components/AddHabitForm";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Typography component="h1" variant="h2" align="center">
           Habit Tracker
         </Typography>
+        <AddHabitForm />
       </Container>
     </Provider>
   );
--- src/store/habitSlice.ts
+++ src/store/habitSlice.ts
@@ -1,28 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface Habit {
-    id: string;
-    name: string;
-    frequency: "daily" | "weekly";
-    completeDates: string[];
-    createdAt: string;
+  id: string;
+  name: string;
+  frequency: "daily" | "weekly";
+  completeDates: string[];
+  createdAt: string;
 }
 
 interface HabitState {
-    habits: Habit[];
+  habits: Habit[];
 }
 
 const initialState: HabitState = {
-    habits: [],
-}
+  habits: [],
+};
 
 const habitSlice = createSlice({
-    name:"habits",
-    initialState,
-    reducers: {
-        addHabit: () => {},
+  name: "habits",
+  initialState,
+  reducers: {
+    addHabit: (
+      state,
+      action: PayloadAction<{ name: string; frequency: "daily" | "weekly" }>
+    ) => {
+      const newHabit: Habit = {
+        id: Date.now().toString(),
+        name: action.payload.name,
+        frequency: action.payload.frequency,
+        completeDates: [],
+        createdAt: new Date().toISOString(),
+      };
+      state.habits.push(newHabit);
     },
-})
+  },
+});
 
-export const {addHabit} = habitSlice.actions;
+export const { addHabit } = habitSlice.actions;
 export default habitSlice.reducer;
